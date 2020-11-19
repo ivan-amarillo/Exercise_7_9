a805bb4bdb082d2df460762b192ab234b2182023
ivan <amarillo20041@gmail.com>
2020-11-19 14:08:13 +0100

    Clean files

--- average.cs
+++ average.cs
@@ -1,20 +1,32 @@
+//Iván Amarillo Rico
+//Software: Calculate the average of a set of marks, until types "end"
+
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+public class Average
+{
+    public static void Main()
+    {
+        double sum = 0, number, counter = 0, average;
+        string mark;
+        
+        // we read the mark as string to check the word "end"
+        Console.Write("Insert a mark: ");
+        mark = Console.ReadLine(); 
+        
+        while(mark != "end")
+        {
+            //convert string to double
+            number = Convert.ToDouble(mark);
+            counter++;
+            sum += number;
+            if(counter != 0)
+            {
+                average = sum / counter;
+                Console.WriteLine("The average is {0}.", average);
+            }
+            Console.Write("Insert a mark: ");
+            mark = Console.ReadLine();
+        }
+    }
 }
